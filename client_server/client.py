86615c10e7011814b13eec04904b2f58bc8874d0
Yvan Gihoza <44585537+YvanGihoza@users.noreply.github.com>
2019-11-14 15:49:44 -0600

    Update client.py

--- client_server/client.py
+++ client_server/client.py
@@ -1,1 +1,32 @@
+import socket
 
+# port of connection 
+port = 23236
+
+# create a socket object 
+s = socket.socket()
+
+
+# connect to the server
+s.connect(('127.0.0.1', port))
+
+while True:
+
+   # provide user the different choices
+   print("Type 1 to Deposit")
+   print("Type 2 to Withdrawl")
+   print("Type 3 to Check balance")
+   print("Type any other key to quit")
+
+   # get input 
+   makeChoice = input()
+   userChoice = str.encode(str(makeChoice), 'utf-8')
+   s.send(userChoice)
+   print(s.recv(1024))
+
+   makeChoice = input()
+   userChoice = str.encode(str(makeChoice), 'utf-8')
+   s.send(userChoice)
+   print(s.recv(1024))
+
+s.close
