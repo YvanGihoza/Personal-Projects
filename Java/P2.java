de5f969ffaca87656d9b851ceb5186ac7bc2cdb1
Yvan Gihoza <44585537+YvanGihoza@users.noreply.github.com>
2020-01-09 12:20:02 -0600

    Update P2.java

--- Java/P2.java
+++ Java/P2.java
@@ -4,7 +4,7 @@ import javax.swing.JLabel;
 public class MoonCake {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		  ImageIcon icon = new ImageIcon("/Users/y.g/eclipse-workspace/Mario Brothers/src/mooncake.jpeg");
+		  ImageIcon icon = new ImageIcon("mooncake.jpeg");  //! Use the correct directory
 		  JLabel label = new JLabel(icon);
 		  frame.add(label);
 		  frame.setDefaultCloseOperation
